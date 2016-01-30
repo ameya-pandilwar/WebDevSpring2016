4c3a7775eb125f22095645efdc0fc9d50121f38f
Ameya Pandilwar <ameya@ccs.neu.edu>
2016-01-30 18:23:03 -0500

    Restructured server.js for assignment and installed path dependency

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/public'));
+var path = require('path');
+
+var public_folder = __dirname + '/public';
+
+app.use(express.static(public_folder));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 app.get('/hello', function(req, res){
     res.send('hello world');
 });
+
+app.get('/assignment', function(req, res){
+   res.sendfile(public_folder + '/assignment/home.html');
+});
+
 app.listen(port, ipaddress);
