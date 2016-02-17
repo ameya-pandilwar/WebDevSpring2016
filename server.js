51d155df120ab26393bde2f4a348fdb6dfcf5a24
Ameya Pandilwar <ameya@pandilwar.com>
2016-02-17 00:17:18 -0500

    Moved the views folder inside the assignment directory

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/hello', function(req, res){
 });
 
 app.get('/assignment', function(req, res){
-   res.sendfile(public_folder + '/assignment/home.view.html');
+   res.sendfile(public_folder + '/assignment/views/home/home.view.html');
 });
 
 app.get('/experiment', function(req, res){
