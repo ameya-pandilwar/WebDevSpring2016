9455c0f1d4f9288144e402c6985bfb7bc6db8c35
Ameya Pandilwar <ameya@pandilwar.com>
2016-02-20 23:59:22 -0500

    Added a MainController

---
+++ public/assignment/views/main/main.controller.js
@@ -0,0 +1,13 @@
+/**
+ * Created by ameyapandilwar on 2/20/16.
+ */
+
+(function(){
+    angular
+        .module("FormBuilderApp")
+        .controller("MainController", MainController)
+
+    function MainController($scope, $location){
+
+    }
+})();
