d36dada215a158b413ce177b94b0cc45ef20ab44
Ameya Pandilwar <ameya@pandilwar.com>
2016-02-22 14:16:21 -0500

    Create forms service

---
+++ public/assignment/services/forms.service.client.js
@@ -0,0 +1,38 @@
+/**
+ * Created by ameyapandilwar on 2/22/16.
+ */
+
+(function(){
+    angular
+        .module("FormBuilderApp")
+        .factory("FormService", FormService);
+
+    function FormService() {
+        var forms = []
+
+        forms = [
+                    {"_id": "000", "title": "Contacts", "userId": 123},
+                    {"_id": "010", "title": "ToDo",     "userId": 123},
+                    {"_id": "020", "title": "CDs",      "userId": 234},
+                ];
+
+        var service = {
+            createFormForUser: createFormForUser,
+            findAllFormsForUser: findAllFormsForUser,
+            deleteFormById: deleteFormById,
+            updateFormById: updateFormById
+        };
+
+        return service;
+
+        function createFormForUser(userId, form, callback){}
+
+        function findAllFormsForUser(userId, callback){}
+
+        function deleteFormById(formId, callback){}
+
+        function updateFormById(formId, newForm, callback){}
+
+    }
+
+})();
