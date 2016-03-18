7387588ac1edee2c60a533f252bd8dd639198dd7
Ameya Pandilwar <ameya@pandilwar.com>
2016-03-18 19:18:11 -0400

    Installed multer dependency

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var bodyParser   = require('body-parser');
 var cookieParser = require('cookie-parser');
 var express      = require('express');
+var multer       = require('multer')
 var session      = require('express-session');
 
 var app = express();
@@ -13,6 +14,7 @@ var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 var session_secret = 'webdev2016';
 
+app.use(multer());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
