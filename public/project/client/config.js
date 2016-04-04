2117e19e37f52f2f3e01485e718b1c0a68111ba9
Ameya Pandilwar <ameya@pandilwar.com>
2016-04-04 19:08:56 -0400

    Updated project config.js

--- public/project/client/config.js
+++ public/project/client/config.js
@@ -11,40 +11,88 @@
     function configuration($routeProvider) {
         $routeProvider
             .when("/home", {
-                templateUrl: "client/views/home/home.view.html",
+                templateUrl: "./views/home/home.view.html",
                 controller: "HomeController"
             })
             .when("/admin", {
-                templateUrl: "client/views/admin/admin.view.html",
-                controller: "AdminController"
+                templateUrl: "./views/admin/admin.view.html",
+                controller: "AdminController",
+                controllerAs: "model"
             })
             .when("/login", {
-                templateUrl: "client/views/users/login.view.html",
-                controller: "LoginController"
+                templateUrl: "./views/users/login.view.html",
+                controller: "LoginController",
+                controllerAs: "model"
             })
             .when("/register", {
-                templateUrl: "client/views/users/register.view.html",
-                controller: "RegisterController"
+                templateUrl: "./views/users/register.view.html",
+                controller: "RegisterController",
+                controllerAs: "model"
             })
             .when("/profile", {
-                templateUrl: "client/views/users/profile.view.html",
-                controller: "ProfileController"
+                templateUrl: "./views/users/profile.view.html",
+                controller: "ProfileController",
+                controllerAs: "model"
             })
-            .when("/module", {
-                templateUrl: "client/views/modules/module.view.html",
-                controller: "ModuleController"
+            .when("/course/:id/module", {
+                templateUrl: "./views/modules/module.view.html",
+                controller: "ModuleController",
+                controllerAs: "model"
             })
-            .when("/module/:id", {
-                templateUrl: "client/views/modules/detail.view.html",
-                controller: "ModuleController"
+            .when("/course/:id/module/:id", {
+                templateUrl: "./views/modules/detail.view.html",
+                controller: "ModuleController",
+                controllerAs: "model"
             })
             .when("/course", {
-                templateUrl: "client/views/courses/brochure.view.html",
-                controller: "BrochureController"
+                templateUrl: "./views/courses/brochure.view.html",
+                controller: "BrochureController",
+                controllerAs: "model"
             })
             .when("/course/:id", {
-                templateUrl: "client/views/courses/course.view.html",
-                controller: "CourseController"
+                templateUrl: "./views/courses/course.view.html",
+                controller: "CourseController",
+                controllerAs: "model"
+            })
+            .when("/course/:id/syllabus", {
+                templateUrl: "./views/courses/syllabus.view.html",
+                controller: "CourseController",
+                controllerAs: "model"
+            })
+            .when("/course/:id/module/:id/lectures", {
+                templateUrl: "./views/modules/lecture.view.html",
+                controller: "ModuleController",
+                controllerAs: "model"
+            })
+            .when("/course/:id/module/:id/lecture/:id", {
+                templateUrl: "./views/modules/lecture.view.html",
+                controller: "ModuleController",
+                controllerAs: "model"
+            })
+            .when("/course/:id/module/:id/examples", {
+                templateUrl: "./views/modules/example.view.html",
+                controller: "ModuleController",
+                controllerAs: "model"
+            })
+            .when("/course/:id/module/:id/example/:id", {
+                templateUrl: "./views/modules/example.view.html",
+                controller: "ModuleController",
+                controllerAs: "model"
+            })
+            .when("/course/:id/module/:id/assignments", {
+                templateUrl: "./views/modules/assignment.view.html",
+                controller: "ModuleController",
+                controllerAs: "model"
+            })
+            .when("/course/:id/module/:id/assignment/:id", {
+                templateUrl: "./views/modules/assignment.view.html",
+                controller: "ModuleController",
+                controllerAs: "model"
+            })
+            .when("/:username/courses", {
+                templateUrl: "./views/users/courses.view.html",
+                controller: "UserController",
+                controllerAs: "model"
             })
             .otherwise({
                 redirectTo: "/home"
