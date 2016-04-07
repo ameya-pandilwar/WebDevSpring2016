b59a8e560120bca87ff54464d22218d0e7e47911
Ameya Pandilwar <ameya@pandilwar.com>
2016-04-07 19:44:34 -0400

    Update server app.js

--- public/project/server/app.js
+++ public/project/server/app.js
@@ -2,9 +2,9 @@
  * Created by ameyapandilwar on 3/8/16.
  */
 
-module.exports = function(app) {
-    var courseModel = require("./models/course.model.js")();
-    var userModel = require("./models/user.model.js")();
+module.exports = function(app, db, mongoose) {
+    var courseModel = require("./models/course.model.js")(db, mongoose);
+    var userModel = require("./models/user.model.js")(db, mongoose);
 
     var courseService = require("./services/course.service.server.js")(app, courseModel);
     var userService = require("./services/user.service.server.js")(app, userModel);
