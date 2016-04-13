92718c144e56ad072d33bf44b671323b31a426c1
Ameya Pandilwar <ameya@pandilwar.com>
2016-04-12 23:05:16 -0400

    Make changes to login view and controller

--- public/assignment/client/views/users/login.controller.js
+++ public/assignment/client/views/users/login.controller.js
@@ -16,10 +16,17 @@
             if (!vm.username && !vm.password) {
                 return;
             }
-            UserService.findUserByCredentials(vm.username, vm.password).then(function(response) {
-                UserService.setCurrentUser(response.data);
-                $location.url('/profile');
-            });
+            UserService.login(vm.username, vm.password).then(
+                function(response) {
+                    if (response) {
+                        UserService.setCurrentUser(response.data);
+                        $location.url('/profile');
+                    }
+                },
+                function(err) {
+                    vm.error = "Username/Password is incorrect or doesn't exist";
+                }
+            );
         }
     }
 }());
