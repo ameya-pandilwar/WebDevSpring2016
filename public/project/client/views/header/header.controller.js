4f6be06835a1af5edf4621958b5a2c1485f41013
Ameya Pandilwar <ameya@pandilwar.com>
2016-04-23 00:50:22 -0400

    Enhancements

--- public/project/client/views/header/header.controller.js
+++ public/project/client/views/header/header.controller.js
@@ -9,8 +9,27 @@
         .controller("HeaderController", HeaderController);
 
     function HeaderController($scope, $location, UserService) {
+        var vm = this;
+        vm.getTabClass = getTabClass;
+
+        $scope.showDropdownOnPage = showDropdownOnPage;
         $scope.showDropdown = showDropdown;
         $scope.logout = logout;
+
+        function getTabClass(currentCourse) {
+            if (currentCourse) {
+                var pattern = '/course/' + currentCourse.number;
+                if ($location.url().indexOf(pattern) > -1) {
+                    return 'active dropdown';
+                }
+            } else {
+                return 'dropdown';
+            }
+        }
+
+        function showDropdownOnPage() {
+            return ($location.url() === '/home' || $location.url() === '/course');
+        }
 
         function showDropdown() {
             return ($location.url() === '/home');
